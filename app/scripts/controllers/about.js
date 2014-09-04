6c781396a9557fa08896653efe8f4e3cf6b54645
bastien03 <bastienmary@gmail.com>
2014-09-04 22:06:55 +0200

    angularjs initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hamilcarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hamilcarApp
+ */
+angular.module('hamilcarApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hamilcarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hamilcarApp
+ */
+angular.module('hamilcarApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
